a3fdece3efd5ae6a591f0569b6d9f62df34e9f4a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-02-12 18:43:37 -0300

    :sparkles: Implementando contexto do bd contendo todos os models

--- src/video-portal/Repository/VideoPortalContext.cs
+++ src/video-portal/Repository/VideoPortalContext.cs
@@ -5,6 +5,36 @@ namespace video_portal.Repository;
 
 public class VideoPortalContext : DbContext, IVideoPortalContext
 {
-    public VideoPortalContext(DbContextOptions<VideoPortalContext> options) : base(options) { }
-    public VideoPortalContext() { }
+  public DbSet<Channel> Channels { get; set; }
+  public DbSet<Video> Videos { get; set; }
+  public DbSet<Comment> Comments { get; set; }
+  public DbSet<User> Users { get; set; }
+  public VideoPortalContext(DbContextOptions<VideoPortalContext> options) : base(options)
+  {
+    Channels = Set<Channel>();
+
+    Videos = Set<Video>();
+
+    Comments = Set<Comment>();
+
+    Users = Set<User>();
+  }
+  public VideoPortalContext()
+  {
+    Channels = Set<Channel>();
+
+    Videos = Set<Video>();
+
+    Comments = Set<Comment>();
+
+    Users = Set<User>();
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (!optionsBuilder.IsConfigured)
+    {
+      optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=master;User=SA;Password=Password12!;");
+    }
+  }
 }
